0410b753d9145a2e1d53139f47bf1b6fd6a5325b
Tebanqito <yemesesteban@gmail.com>
2022-06-07 15:43:03 -0500

    Primer commit

---
+++ Ej1.py
@@ -0,0 +1,6 @@
+edad = int(input("Ingrese su edad: "))
+
+if (edad < 18):
+    print("Usted tiene ", edad, " anios y es menor de edad")
+else:
+    print("Usted tiene ", edad, " anios y es mayor de edad")
